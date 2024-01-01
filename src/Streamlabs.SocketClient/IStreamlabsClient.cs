31f5b9fe2a042769284bdb3a8b2bc6fff27cd324
Samuel Meenzen <samuel@meenzen.net>
2024-01-01 14:05:10 +0100

    docs: add documentation for IStreamlabsClient

--- src/Streamlabs.SocketClient/IStreamlabsClient.cs
+++ src/Streamlabs.SocketClient/IStreamlabsClient.cs
@@ -5,17 +5,32 @@ namespace Streamlabs.SocketClient;
 
 public interface IStreamlabsClient : IDisposable
 {
+    /// <summary>
+    /// Connects to the Streamlabs websocket API.
+    /// </summary>
     Task ConnectAsync();
+
+    /// <summary>
+    /// Disconnects from the Streamlabs websocket API.
+    /// </summary>
     Task DisconnectAsync();
 
     /// <summary>
     /// Allows executing the entire event pipeline using a raw json payload.
     /// </summary>
-    /// <param name="json"></param>
+    /// <param name="json">The raw json payload.</param>
     void Dispatch(string json);
 
+    /// <summary>
+    /// Fires when a raw json payload is received from the websocket.
+    /// </summary>
     event EventHandler<string>? OnEventRaw;
+
+    /// <summary>
+    /// Fires for each event contained in the json payload after it has been successfully deserialized.
+    /// </summary>
     event EventHandler<IStreamlabsEvent>? OnEvent;
+
     event EventHandler<DonationMessage>? OnDonation;
     event EventHandler<BitsMessage>? OnBits;
     event EventHandler<DonationDeleteMessage>? OnDonationDelete;
